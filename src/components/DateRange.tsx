780ca8d216a1911695830e4dc6bfd0230f53b613
KavinduLakshitha <kavindu.m.online@gmail.com>
2026-06-10 07:13:26 +0530

    ###

--- src/components/DateRange.tsx
+++ src/components/DateRange.tsx
@@ -85,7 +85,7 @@ export function DatePickerWithRange({
         </PopoverTrigger>
         <PopoverContent className="w-auto p-0" align="start">
           <Calendar
-            initialFocus
+            autoFocus
             mode="range"
             defaultMonth={displayRange?.from}
             selected={selected || (dateRange ? {
